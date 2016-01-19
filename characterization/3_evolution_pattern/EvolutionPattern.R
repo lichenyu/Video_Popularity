d530734906cbf834911c72853f737eec43cb153a
babytuan <ouyangshuxin@gmail.com>
2016-01-20 00:32:56 +0800

    plot each kind of pattern

---
+++ characterization/3_evolution_pattern/EvolutionPattern.R
@@ -0,0 +1,55 @@
+#workpath = 'F:/Video_Popularity/'
+workpath = '/Users/ouyangshuxin/Documents/Video_Popularity/'
+
+data = read.table(paste(workpath, 'characterization/3_evolution_pattern/data', sep = ''))
+
+
+
+pdf(paste(workpath, "characterization/3_evolution_pattern/patterns.pdf", sep = ''), 
+    width = 7.5, height = 4)
+
+par(mfrow=c(2, 3))
+
+#d,l,u,r
+par(mar=c(4, 5, 2, 2) + 0.1)
+plot(seq(0, 30), data[1, ] / data[1, 31], type = "l", 
+     xlim = c(0, 30), ylim = c(0, 1), 
+     xlab = "Days", ylab = "Normalized View Count", main = "Burst-Slow", 
+     col = "blue", lwd = 3)
+
+#d,l,u,r
+par(mar=c(4, 5, 2, 2) + 0.1)
+plot(seq(0, 30), data[2, ] / data[2, 31], type = "l", 
+     xlim = c(0, 30), ylim = c(0, 1), 
+     xlab = "Days", ylab = "Normalized View Count", main = "Burst-Slow-Burst-Slow", 
+     col = "blue", lwd = 3)
+
+#d,l,u,r
+par(mar=c(4, 5, 2, 2) + 0.1)
+plot(seq(0, 30), data[3, ] / data[3, 31], type = "l", 
+     xlim = c(0, 30), ylim = c(0, 1), 
+     xlab = "Days", ylab = "Normalized View Count", main = "Burst-Slow-Burst-Slow-Burst-Slow", 
+     col = "blue", lwd = 3)
+
+#d,l,u,r
+par(mar=c(4, 5, 2, 2) + 0.1)
+plot(seq(0, 30), data[4, ] / data[4, 31], type = "l", 
+     xlim = c(0, 30), ylim = c(0, 1), 
+     xlab = "Days", ylab = "Normalized View Count", main = "Slow-Burst-Slow", 
+     col = "blue", lwd = 3)
+
+#d,l,u,r
+par(mar=c(4, 5, 2, 2) + 0.1)
+plot(seq(0, 30), data[5, ] / data[5, 31], type = "l", 
+     xlim = c(0, 30), ylim = c(0, 1), 
+     xlab = "Days", ylab = "Normalized View Count", main = "Slow-Burst-Slow-Burst-Slow", 
+     col = "blue", lwd = 3)
+
+#d,l,u,r
+par(mar=c(4, 5, 2, 2) + 0.1)
+plot(seq(0, 30), data[6, ] / data[6, 31], type = "l", 
+     xlim = c(0, 30), ylim = c(0, 1), 
+     xlab = "Days", ylab = "Normalized View Count", main = "Steady", 
+     col = "blue", lwd = 3)
+
+dev.off()
